603e787a1e46ffff660f29333f9066ef4f9a63b7
nebulau <16061196@buaa.edu.cn>
2018-11-24 14:39:13 +0800

    modify little

--- second.py
+++ second.py
@@ -1,5 +1,7 @@
 import imp
 from numpy import loadtxt
+#利用函数参数来设置模型参数，使用sklearn接口
+#XGBClassifier是xgboost的sklearn包
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -22,10 +24,19 @@ Y = train['label'][0:2200]
 
 
 seed = 8
-test_size = 0
+test_size = 0 #拆分比例
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
 
-model = XGBClassifier()
+model = XGBClassifier(
+    silent = 1,#打印运行消息default = 0
+    max_child_weight = 1,#孩子节点最小样本权重和default = 1
+    max_depth = 10,#定义一棵树的最大深度default = 6
+    gamma = 0,#指定了分割时所需的最小损失的减少量default = 0
+    max_delta_step = 0,#在逻辑回归中，当类别比例非常不平衡才起作用
+    subsample = 1,#较低的值使算法比较保守，防止过拟合
+    alpha = 0,#维数较高时使用，加快运行速度
+    seed = 0,#种子随机数，etc其他参数
+    )
    
 
 
@@ -34,9 +45,9 @@ model.fit(X_train, y_train)
 
 
 predictions = model.predict(test_X)
+#predictions = [value for value in model.predict(X_test)]
 dataframe = pd.DataFrame(predictions)
-#for data in predictions:
-    #writer.writerow(str(data))
+
 dataframe.to_csv("results.csv", index=False, sep = ',',header = None)
 print("Finished.")
 #print(predictions)
